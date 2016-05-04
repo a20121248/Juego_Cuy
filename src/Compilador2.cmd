86050e06c9873c40ef8192a28edccc33d461f794
Lorena Monzón <Lorena Monzn@Lorena-PC>
2016-05-04 01:37:29 -0500

    Juego_Cuy_lab3_v3 - Anthony

--- src/Compilador2.cmd
+++
@@ -1,7 +0,0 @@
-REM set path=%path%;C:\Program Files\Java\jdk1.8.0_40\bin
-set path=%path%;C:\Program Files\Java\jdk1.7.0_79\bin
-javac Model/*.java 
-jar cf Libreria.jar Model/*.class 
-javac Controller/*.java View/*.java 
-jar cvfm Juego_Cuy.jar MANIFEST.MF Controller/*.class View/*.class  *.txt
-pause
--- src/View/Renderizador.java
+++ src/View/Renderizador.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 /**
@@ -37,5 +38,46 @@ public class Renderizador {
         graphics.drawImage(imgP1, p1.getPosX()*MAX_SIZE, p1.getPosY()*MAX_SIZE,MAX_SIZE,MAX_SIZE,null);
         graphics.drawImage(imgP2, p2.getPosX()*MAX_SIZE, p2.getPosY()*MAX_SIZE,MAX_SIZE,MAX_SIZE,null);
     }
+    
+    /* Muestra los datos (nombres, vida, nivel) */
+    public void pnlTexto_mostrarDatos(Graphics graphics, Personaje p1, Personaje p2) throws IOException {
+        String vida = "Vida : " + p1.getVida();
+        String nombre1  = "Nombre Jugador 1: " + p1.getNombre();
+        String nombre2  = "Nombre Jugador 2: " + p2.getNombre();
+        
+        String accion = p1.getAccionEspecial(0);
+        
+        graphics.drawString(vida, 15, 50);
+        
+//        graphics.drawString(accion, 15, 60);
+        
+
+    }
+    
+    public void pnlTexto_mostrarLista(Graphics graphics, List<String> lstString, int inicial, int incremento) {
+
+        int x = 15, y = inicial;
+        int linea_length = 0;
+        String linea_acumulador = "";
+        for (int i = 0; i < lstString.size(); ++i) {
+            String[] lstPalabras = lstString.get(i).split(" ");
+            linea_length = 0;
+            linea_acumulador = "";
+            for (int j = 0; j < lstPalabras.length; ++j) {
+                linea_acumulador += (" " + lstPalabras[j]);
+                linea_length += (1 + lstPalabras[j].length());
+                
+                boolean cond1 = linea_length >= 25;
+                boolean cond2 = (j == lstPalabras.length-1);
+                if (cond1 || cond2) {
+                    graphics.drawString(linea_acumulador, x, y);
+                    linea_length = 0;
+                    linea_acumulador = "";
+                    y += incremento/2;
+                }
+            }
+            y += incremento;
+        }
+    }      
     
 }
